949b889c1f8d52d2174e53b3feaaca639a4b8aa5
ChauBacHozz <phongndsaiyan@gmail.com>
2022-03-19 22:52:35 +0700

    fixing css task

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,27 @@ const button = document.querySelector('.button');
 const task_zone = document.querySelector('.task-zone');
 // console.log(task_zone);
 
-button.onclick = function() {
-    let content = input.value;
-    input.value = '';
-    const iDiv = document.createElement('div');
-    iDiv.className = 'task';
-    iDiv.innerHTML = content;
-    task_zone.appendChild(iDiv);
+function blankCheck(content) {
+    return content.trim().length === 0;
 }
+
+button.onclick = function() {
+    if (!blankCheck(input.value)) {
+        let content = input.value;
+        input.value = '';
+        let iDiv = document.createElement('div');
+        iDiv.className = 'task';
+        let check_Div = document.createElement('span');
+        check_Div.className = 'task-check ti-check';
+        iDiv.appendChild(check_Div);
+        let text_Div = document.createElement('div');
+        text_Div.className = 'task-content';
+        text_Div.innerHTML = content;
+        iDiv.appendChild(text_Div);
+        let trash_Div = document.createElement('span');
+        trash_Div.className = 'task-trash ti-trash';
+        iDiv.appendChild(trash_Div);
+        task_zone.appendChild(iDiv);
+    }
+}
+
