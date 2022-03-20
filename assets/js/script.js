fb1dded68fdecccb37a5137392f58e6960be0e32
ChauBacHozz <phongndsaiyan@gmail.com>
2022-03-20 10:35:30 +0700

    fixed float right trash icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,15 +13,21 @@ button.onclick = function() {
         input.value = '';
         let iDiv = document.createElement('div');
         iDiv.className = 'task';
-        let check_Div = document.createElement('span');
-        check_Div.className = 'task-check ti-check';
+        let check_Div = document.createElement('div');
+        check_Div.className = 'task-check';
+        let check_icon = document.createElement('i');
+        check_icon.className = 'ti-check';
+        check_Div.appendChild(check_icon); 
         iDiv.appendChild(check_Div);
         let text_Div = document.createElement('div');
         text_Div.className = 'task-content';
         text_Div.innerHTML = content;
         iDiv.appendChild(text_Div);
-        let trash_Div = document.createElement('span');
-        trash_Div.className = 'task-trash ti-trash';
+        let trash_Div = document.createElement('div');
+        trash_Div.className = 'task-trash';
+        let trash_icon = document.createElement('i');
+        trash_icon.className = 'ti-trash';
+        trash_Div.appendChild(trash_icon); 
         iDiv.appendChild(trash_Div);
         task_zone.appendChild(iDiv);
     }
